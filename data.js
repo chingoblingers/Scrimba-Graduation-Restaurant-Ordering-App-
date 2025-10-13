39a51f4d4499b17bdd2ee2b7a7dc308e96584658
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2025-10-12 22:19:17 -0500

    created html,css, + java files and typed starting boilerplate for each

---
+++ data.js
@@ -0,0 +1,23 @@
+const menuArray = [
+    {
+        name: "Dai-Katana",
+        catagory: ["steel", "two-handed", "close-quarters",],
+        id: 0,
+        price: 1400,
+        emoji: "⚔️"
+    },
+    {
+        name: "Dagger",
+        catagory: ["steel", "one-handed", "close-quarters", "throwable"],
+        price: 80,
+        emoji: "🗡️",
+        id: 1
+    },
+        {
+        name: "Smoke-bomb",
+        catagory: ["throwable", "explosive", "long-range"],
+        price: 12,
+        emoji: "💣",
+        id: 2
+    }
+]
