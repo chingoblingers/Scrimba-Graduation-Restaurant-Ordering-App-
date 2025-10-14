0d3e66d5c90cba6febe99700f673a5bff4a3f983
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2025-10-13 20:55:21 -0500

    corrected script link and moved weapon section to Javascript

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 import {menuArray} from "./data.js"
+const weaponContainer = document.getElementById("weapon-container")
+
+function renderWeapon(wepArr){
+    const weaponMenu = wepArr.map(menu =>{
+       const {name,catagory,id,price,emoji} = menu
+       return `
+       <section id="weapon">
+
+    <p class="weapon-img" id="${id} "> ${emoji} </p>
+
+    <div class="weapon-info">
+        <h2> ${name}  </h2>
+        <p> ${catagory} </p>
+        <p class="cost"> ${price}$  </p>   
+    </div>
+
+    <button class="purchase-weapon" data-add="${id}"> + </button>
+
+</section>
+       ` 
+    }).join(" ")
+
+    weaponContainer.innerHTML+= weaponMenu
+}
+
+renderWeapon(menuArray)
