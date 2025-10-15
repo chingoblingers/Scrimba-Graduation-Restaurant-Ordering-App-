7ccf1eef1ef657d56481997d9325e749fffad632
Kenton Jackson <130018058+chingoblingers@users.noreply.github.com>
2025-10-14 21:55:33 -0500

    created functions to add items to cart

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import {menuArray} from "./data.js"
 const weaponGallery = document.getElementById("weapon-gallery")
 const cart = document.getElementById("cart-items")
+let weaponBasket = []
 
 function generateWeaponHtml(wepArr){
    const weaponMenu = wepArr.map(menu =>{
@@ -8,7 +9,7 @@ function generateWeaponHtml(wepArr){
        return `
        <section class="weapon">
 
-    <p class="weapon-img" id="${id}"> ${emoji} </p>
+    <p class="weapon-img"> ${emoji} </p>
 
     <div class="weapon-info">
         <h2> ${name}  </h2>
@@ -30,3 +31,39 @@ function renderWeaponList(arr){
 }
 
 renderWeaponList(menuArray)
+
+
+function addToBasket(id, arr){
+  arr.forEach(wep => {
+    if(wep.id === id){
+        weaponBasket.push(wep)
+    }
+  }); 
+renderBasket()
+}
+
+function generateBaskethtml(){
+  const finalBasket = weaponBasket.map(wep =>{
+        return ` 
+<div class="cart-details">
+<h2> ${wep.name} </h2>
+<button> remove </button>
+<p> ${wep.price}</p>
+</div>
+        `
+    }).join(" ")
+   return finalBasket
+
+}
+
+function renderBasket(){
+    cart.innerHTML = generateBaskethtml()
+}
+
+weaponGallery.addEventListener("click", e =>{
+    const selected = Number(e.target.dataset.add)
+    if(!isNaN(selected)){
+        addToBasket(selected, menuArray)
+    }
+})
+
